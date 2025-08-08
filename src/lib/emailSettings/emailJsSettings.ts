b3241c04dcf893cf1a96fd8ad566c8f78ebe1263
Gulsare Mirzayeva <136263823+GulsareMirzayeva@users.noreply.github.com>
2025-08-09 02:19:16 +0400

    Add files via upload

---
+++ src/lib/emailSettings/emailJsSettings.ts
@@ -0,0 +1,12 @@
+type emailJSTypes = {
+  serviceId: string;
+  templateId: string;
+  publicKey: string;
+};
+
+export const emailJSKeys: emailJSTypes = {
+  serviceId: 'service_zr8wqen', // EmailJS Service ID: 't Hartje
+  // serviceId: 'service_36vj7wq', // EmailJS Service ID: To test form on own emmail-adress
+  templateId: 'template_rpt090a', // EmailJS Template ID
+  publicKey: 'M_xi1jBN2xoetyO05', // EmailJS Public Key
+};
