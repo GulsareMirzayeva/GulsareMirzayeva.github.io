eb0167b8a8177b737dd17d9c61618964b6f4fa17
Gulsare Mirzayeva <136263823+GulsareMirzayeva@users.noreply.github.com>
2025-08-09 02:20:22 +0400

    Add files via upload

---
+++ src/data/pages/pagesContent.ts
@@ -0,0 +1,60 @@
+import {
+  cardsContentPaths,
+  copyPrintContentPaths,
+  customClotingContentPaths,
+  flyersContentPaths,
+  foldersContentPaths,
+  postersContentPaths,
+  stickersContentPaths,
+} from '../../lib/translationPaths';
+
+export const stickersPageContent = {
+  pageTitle: stickersContentPaths.pageHeader.title,
+  pageIntroduction: stickersContentPaths.pageHeader.introduction,
+  pageLink: '/custom-print/stickers',
+  image: stickersContentPaths.data.image,
+};
+
+export const cardsPageContent = {
+  pageTitle: cardsContentPaths.pageHeader.title,
+  pageIntroduction: cardsContentPaths.pageHeader.introduction,
+  pageLink: '/custom-print/cards',
+  image: cardsContentPaths.data.image,
+};
+
+export const flyersPageContent = {
+  pageTitle: flyersContentPaths.pageHeader.title,
+  pageIntroduction: flyersContentPaths.pageHeader.introduction,
+  pageLink: '/custom-print/flyers',
+  image: flyersContentPaths.data.image,
+};
+
+export const foldersPageContent = {
+  pageTitle: foldersContentPaths.pageHeader.title,
+  pageIntroduction: foldersContentPaths.pageHeader.introduction,
+  pageLink: '/custom-print/folders',
+  image: foldersContentPaths.data.image,
+};
+
+export const postersPageContent = {
+  pageTitle: postersContentPaths.pageHeader.title,
+  pageIntroduction: postersContentPaths.pageHeader.introduction,
+  pageLink: '/custom-print/posters',
+  image: postersContentPaths.data.image,
+};
+
+export const copyPrintPageContent = {
+  pageTitle: copyPrintContentPaths.pageHeader.title,
+  pageIntroduction: copyPrintContentPaths.pageHeader.introduction,
+  pageSubTitle: copyPrintContentPaths.pageHeader.subTitle,
+  pageSubText: copyPrintContentPaths.pageHeader.subText,
+  pageLink: '/copy-print',
+  image: copyPrintContentPaths.data.image,
+};
+
+export const customClothingPageContent = {
+  pageTitle: customClotingContentPaths.pageHeader.title,
+  pageIntroduction: customClotingContentPaths.pageHeader.introduction,
+  pageLink: '/copy-print',
+  image: customClotingContentPaths.data.image,
+};
