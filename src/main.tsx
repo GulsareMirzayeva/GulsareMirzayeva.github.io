a7afb5041875eb063d2126c2c38ad31882e8af87
Gulsare Mirzayeva <136263823+GulsareMirzayeva@users.noreply.github.com>
2025-08-12 22:37:11 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+try {
+  const k = localStorage.getItem('i18nextLng');
+  if (k === 'nl') localStorage.setItem('i18nextLng', 'aze');
+} catch (_) {}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
