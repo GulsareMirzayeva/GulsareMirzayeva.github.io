a9e680b17a77539878efdee7d573ff915379f1e7
Gulsare Mirzayeva <136263823+GulsareMirzayeva@users.noreply.github.com>
2025-08-12 22:41:06 +0400

    Update i18.ts

--- src/utils/i18.ts
+++ src/utils/i18.ts
@@ -1,15 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
-import aze from '../../i18n/aze/translation.json'; 
+import aze from '../../i18n/aze/translation.json';
 import en from '../../i18n/en/translation.json';
 
 i18n.use(initReactI18next).init({
-  lng: 'aze',              
-  fallbackLng: 'en',      
+  lng: 'aze',
+  fallbackLng: 'en',
   resources: {
-    aze: { translation: aze }, 
-    en:  { translation: en  },
+    aze: { translation: aze },
+    en: { translation: en },
   },
   interpolation: { escapeValue: false },
 });
