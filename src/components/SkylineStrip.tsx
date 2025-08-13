f1937c8dab228388e5378a45f5b6b74680fc397f
Gulsare Mirzayeva <136263823+GulsareMirzayeva@users.noreply.github.com>
2025-08-13 17:42:22 +0400

    Update SkylineStrip.tsx

--- src/components/SkylineStrip.tsx
+++ src/components/SkylineStrip.tsx
@@ -3,8 +3,13 @@ import skyline from '../assets/skyline1.png';
 export default function SkylineStrip() {
   return (
     <div
-      className="w-full min-h-32 bg-repeat-x bg-center transition dark:brightness-75 dark:contrast-100"
-      style={{ backgroundImage: `url(${skyline})` }}
+      className="w-full bg-repeat-x bg-bottom transition dark:brightness-75 dark:contrast-100"
+      style={{
+        backgroundImage: `url(${skyline})`,
+        height: 'clamp(90px, 14vw, 160px)',        // ekran ölçüsünə görə avtomatik hündürlük
+        backgroundSize: 'auto clamp(90px, 14vw, 160px)', // şəkil hündürlüyə uyğunlaşdırılır
+        backgroundPosition: 'left bottom',        // soldan və aşağıdan başlasın
+      }}
     ></div>
   );
 }
